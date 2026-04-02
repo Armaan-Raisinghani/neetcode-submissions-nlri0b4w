ab6a446ba077605c1c158f31d238dbd18990b481
Armaan-Raisinghani <raisinghani.armaan1@gmail.com>
2026-04-03 05:10:48 +0530

    Add: permutation-string - submission-0

---
+++ Data Structures & Algorithms/permutation-string/submission-0.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+        int l = 0;
+        int r = 0;
+        vector<int> t(26,0);
+        vector<int> c(26,0);
+        for(const auto& i:s1) t[i-'a']++;
+        while (r-l<s1.size()){
+            c[s2[r]-'a']++;
+            r++;
+        }
+        if (c==t) return true;
+        while (r<s2.size()) {
+            c[s2[r]-'a']++;           
+            for(const auto& i:c) cout<<i<<endl;
+            cout << endl;
+            c[s2[l]-'a']--;
+            l++;
+            r++;
+            if (c==t) return true;
+        }
+        return false;
+    }
+};
